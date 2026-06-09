83cb5c7e9473fd983d60d606fedcfc4237cb72b8
Unnamed00000 <157989745+Unnamed00000@users.noreply.github.com>
2026-06-09 17:54:11 +0200

    Fix hidden modal display

--- app.js
+++ app.js
@@ -344,7 +344,7 @@ const languages = [
   }
 ];
 
-const appVersion = "1.1.2";
+const appVersion = "1.1.3";
 const appLanguages = [
   { id: "ru", name: "Русский", html: "ru" },
   { id: "ka", name: "ქართული", html: "ka" },
